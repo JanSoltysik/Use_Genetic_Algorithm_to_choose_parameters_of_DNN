np.substract fix in fintess calculation

nn_optimizer.py
@@ -147,7 +147,7 @@ class NNOptimize:
 
         weights = np.array([rescale_size_of_nn(weight) for weight in weights])
 
-        performance_score = np.subtract(performance_score, -1)
+        performance_score = np.subtract(performance_score, 1)
         norm_scale = np.linalg.norm(performance_score)
         performance_score = np.divide(performance_score, norm_scale)
 